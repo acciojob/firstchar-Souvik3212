5821142a45c3dfb86ec2068abf03d9308cd92e81
Souvik3212 <125539052+Souvik3212@users.noreply.github.com>
2024-06-26 21:34:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function firstChar(text) {
   // your code here
+	let trimmedText = text.trim();
+    
+    
+    if (trimmedText === '') {
+        return ''; 
+    }
+    
+    
+    for (let i = 0; i < trimmedText.length; i++) {
+        if (trimmedText[i] !== ' ') {
+            return trimmedText[i]; 
+        }
+    }
+
+    
+    return '';
 }
 
 // Do not change the code below
